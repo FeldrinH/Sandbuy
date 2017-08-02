28f39c4a7cf35e21867525f795b3867f8307f614
FeldrinH <fhandswitch@gmail.com>
2017-08-02 20:38:35 +0300

    Fixed buylog containing semicolon separated values instead of comma separated values

--- gamemodes/sandbuy/gamemode/buylogger.lua
+++ gamemodes/sandbuy/gamemode/buylogger.lua
@@ -2,7 +2,7 @@ buylogger = buylogger or {Active=false}
 
 function buylogger.Init()
 	if !file.Exists("buylog.txt", "DATA") then
-		file.Write("buylog.txt", "action;player;target;newmoney\n")
+		file.Write("buylog.txt", "action,player,target,newmoney\n")
 	end
 	
 	buylogger.File = file.Open("buylog.txt", "a", "DATA")
@@ -19,28 +19,28 @@ end
 
 function buylogger.LogKill(ply, victim, newmoney)
 	if buylogger.Active then
-		buylogger.File:Write("kill;" .. ply:Nick() .. ";" .. victim:Nick() .. ";" .. newmoney .. "\n")
+		buylogger.File:Write("kill," .. ply:Nick() .. "," .. victim:Nick() .. "," .. newmoney .. "\n")
 		--buylogger.File:Flush()
 	end
 end
 
 function buylogger.LogDeath(ply, newmoney)
 	if buylogger.Active then
-		buylogger.File:Write("death;" .. ply:Nick() .. ";;" .. newmoney .. "\n")
+		buylogger.File:Write("death," .. ply:Nick() .. ",," .. newmoney .. "\n")
 		--buylogger.File:Flush()
 	end
 end
 
 function buylogger.LogBuy(ply, class, buytype, newmoney)
 	if buylogger.Active then
-		buylogger.File:Write("buy-" .. buytype .. ";" .. ply:Nick() .. ";" .. class .. ";" .. newmoney .. "\n")
+		buylogger.File:Write("buy-" .. buytype .. "," .. ply:Nick() .. "," .. class .. "," .. newmoney .. "\n")
 		buylogger.File:Flush()
 	end
 end
 
 function buylogger.LogBailout(ply, newmoney)
 	if buylogger.Active then
-		buylogger.File:Write("bailout;" .. ply:Nick() .. ";;" .. newmoney .. "\n")
+		buylogger.File:Write("bailout," .. ply:Nick() .. ",," .. newmoney .. "\n")
 		--buylogger.File:Flush()
 	end
 end
