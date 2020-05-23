8335fd6ad3359f7da11b254cd7276fe051cf990e
FeldrinH <fhandswitch@gmail.com>
2020-05-23 21:41:12 +0300

    Optimize logging

--- gamemodes/sandbuy/gamemode/buylogger.lua
+++ gamemodes/sandbuy/gamemode/buylogger.lua
@@ -1,5 +1,7 @@
 buylogger = buylogger or {Active=false}
 
+local logfile = nil
+
 function buylogger.Init()
 	if buylogger.Active then return end
 
@@ -8,13 +10,13 @@ function buylogger.Init()
 		file.CreateDir("buylogs")
 	end
 	
-	if buylogger.File then
+	if logfile then
 		buylogger.LogTimestamped("logging-enabled", "")
 	else
 		if !file.Exists(filename, "DATA") then
 			file.Write(filename, "action,time,player,target,newmoney,deltamoney,killweapon\n")
 		end
-		buylogger.File = file.Open(filename, "a", "DATA")
+		logfile = file.Open(filename, "a", "DATA")
 		buylogger.LogTimestamped("logging-started", game.GetMap())
 	end
 	
@@ -22,13 +24,13 @@ function buylogger.Init()
 end
 
 function buylogger.Close()
-	if !buylogger.File then return end
+	if !logfile then return end
 	
 	buylogger.Active = false
 	
 	buylogger.LogTimestamped("logging-ended", "")
-	buylogger.File:Close()
-	buylogger.File = nil
+	logfile:Close()
+	logfile = nil
 end
 
 local function GetLogTime()
@@ -51,59 +53,59 @@ local FormatActor = buylogger.FormatActor
 
 function buylogger.LogKill(ply, victim, wepname, newmoney, delta, ispenalty)
 	if buylogger.Active then
-		buylogger.File:Write(GetLogTime() .. (ispenalty and ",kill-penalty," or ",kill,") .. FormatPlayer(ply) .. "," .. FormatActor(victim) .. "," .. newmoney .. "," .. delta .. "," .. wepname .. "\n")
+		logfile:Write(GetLogTime() .. (ispenalty and ",kill-penalty," or ",kill,") .. FormatPlayer(ply) .. "," .. FormatActor(victim) .. "," .. newmoney .. "," .. delta .. "," .. wepname .. "\n")
 	end
 end
 
 function buylogger.LogDeath(ply, atk, wepname, newmoney, delta)
 	if buylogger.Active then
 		--if ply == atk then atk = nil end
-		buylogger.File:Write(GetLogTime() .. ",death," .. FormatPlayer(ply) .. "," .. FormatActor(atk) .. "," .. newmoney .. "," .. delta .. "," .. wepname .. "\n")
+		logfile:Write(GetLogTime() .. ",death," .. FormatPlayer(ply) .. "," .. FormatActor(atk) .. "," .. newmoney .. "," .. delta .. "," .. wepname .. "\n")
 	end
 end
 
 function buylogger.LogDestroy(ply, vehclass, newmoney, delta)
 	if buylogger.Active then
-		buylogger.File:Write(GetLogTime() .. ",destroy," .. FormatPlayer(ply) .. "," .. vehclass .. "," .. newmoney .. "," .. delta .. "\n")
+		logfile:Write(GetLogTime() .. ",destroy," .. FormatPlayer(ply) .. "," .. vehclass .. "," .. newmoney .. "," .. delta .. "\n")
 	end
 end
 
 function buylogger.LogBuy(ply, class, buytype, newmoney, delta, amount)
 	if buylogger.Active then
-		buylogger.File:Write(GetLogTime() .. ",buy-" .. buytype .. "," .. FormatPlayer(ply) .. "," .. class .. "," .. newmoney .. "," .. delta .. "," .. (amount or 1) .. "\n")
-		if buytype != "ammo" then
-			buylogger.File:Flush()
-		end
+		logfile:Write(GetLogTime() .. ",buy-" .. buytype .. "," .. FormatPlayer(ply) .. "," .. class .. "," .. newmoney .. "," .. delta .. "," .. (amount or 1) .. "\n")
+		--[[if buytype != "ammo" then
+			logfile:Flush()
+		end]]
 	end
 end
 
 function buylogger.LogBailout(ply, newmoney, delta)
 	if buylogger.Active then
-		buylogger.File:Write(GetLogTime() .. ",bailout," .. FormatPlayer(ply) .. ",," .. newmoney .. "," .. delta .. "\n")
+		logfile:Write(GetLogTime() .. ",bailout," .. FormatPlayer(ply) .. ",," .. newmoney .. "," .. delta .. "\n")
 	end
 end
 
 function buylogger.LogStartingBailout(ply, newmoney, delta)
 	if buylogger.Active then
-		buylogger.File:Write(GetLogTime() .. ",bailout-start," .. FormatPlayer(ply) .. ",," .. newmoney .. "," .. delta .. "\n")
+		logfile:Write(GetLogTime() .. ",bailout-start," .. FormatPlayer(ply) .. ",," .. newmoney .. "," .. delta .. "\n")
 	end
 end
 
 function buylogger.LogReset(resettype, resettarget)
 	if buylogger.Active then
-		buylogger.File:Write(GetLogTime() .. ",reset-" .. resettype .. "," .. resettarget .. "\n")
+		logfile:Write(GetLogTime() .. ",reset-" .. resettype .. "," .. resettarget .. "\n")
 	end
 end
 
 function buylogger.LogString(log_type, message)
 	if buylogger.Active then
-		buylogger.File:Write(GetLogTime() .. "," .. log_type .. "," .. message .. "\n")
+		logfile:Write(GetLogTime() .. "," .. log_type .. "," .. message .. "\n")
 	end
 end
 
 function buylogger.LogTimestamped(log_type, message)
-	if buylogger.File then
-		buylogger.File:Write(GetLogTime() .. "," .. log_type .. "," .. os.date("%H:%M:%S %d.%m.%Y", os.time()) .. "," .. message .. "\n")
+	if logfile then
+		logfile:Write(GetLogTime() .. "," .. log_type .. "," .. os.date("%H:%M:%S %d.%m.%Y", os.time()) .. "," .. message .. "\n")
 	end
 end
 
